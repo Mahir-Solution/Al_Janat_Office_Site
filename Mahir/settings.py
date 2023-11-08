8b9644abcf8775b5e0fc843d028b84e045b2bf70
unknown <wasirajput322@gmail.com>
2023-11-08 14:08:46 +0500

    third update

--- Mahir/settings.py
+++ Mahir/settings.py
@@ -23,11 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2iohku*$68(20^d2cwayp%8c+y797$p*9oq1qc7(v3b9wckel2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+#ALLOWED_HOSTS = ['.vercel.app']
 
-#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 
 # Application definition
 
